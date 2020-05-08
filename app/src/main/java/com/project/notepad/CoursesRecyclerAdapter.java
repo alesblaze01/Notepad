1bd680e92686d585736bfc9e7571f2a17c29bf99
ales-blaze <alesblaze@gmail.com>
2020-05-08 23:57:15 +0530

    Added Navigation Drawer for courses and notes

---
+++ app/src/main/java/com/project/notepad/CoursesRecyclerAdapter.java
@@ -0,0 +1,61 @@
+package com.project.notepad;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.project.notepad.Utility.CourseInfo;
+
+import java.util.List;
+
+public class CoursesRecyclerAdapter extends RecyclerView.Adapter<CoursesRecyclerAdapter.NoteViewHolder> {
+    final public Context mContext;
+    private List<CourseInfo> mCourses;
+
+    public CoursesRecyclerAdapter(Context context, List<CourseInfo> courseInfos) {
+        mContext = context;
+        mCourses = courseInfos;
+    }
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_course_list,parent,false);
+        return new NoteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        CourseInfo courseInfo = mCourses.get(position);
+        holder.mCourseTitle.setText(courseInfo.getTitle());
+        holder.mPosition = position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCourses.size();
+    }
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder{
+        public final TextView mCourseTitle;
+
+        int mPosition;
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mCourseTitle = itemView.findViewById(R.id.text_view_course_list_item);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    Snackbar.make(v.getRootView(),mCourseTitle.getText(),Snackbar.LENGTH_SHORT);
+                    Toast.makeText(mContext, mCourseTitle.getText() , Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/project/notepad/NoteListActivity.java
+++ app/src/main/java/com/project/notepad/NoteListActivity.java
@@ -4,14 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.project.notepad.Utility.CourseInfo;
 import com.project.notepad.Utility.DataManager;
 import com.project.notepad.Utility.NoteInfo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,8 +30,12 @@ import android.widget.ListView;
 
 import java.util.List;
 
-public class NoteListActivity extends AppCompatActivity {
+public class NoteListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private NoteRecyclerAdapter mNoteRecyclerAdapter;
+    private LinearLayoutManager mLinearLayoutManager;
+    private RecyclerView mRecyclerView;
+    private CoursesRecyclerAdapter mCoursesRecyclerAdapter;
+    private GridLayoutManager mGridLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +53,14 @@ public class NoteListActivity extends AppCompatActivity {
             }
         });
 
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_navigation,R.string.close_navigation);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_bar);
+        navigationView.setNavigationItemSelectedListener(this);
+
         initializeDisplayContent();
     }
 
@@ -53,12 +75,68 @@ public class NoteListActivity extends AppCompatActivity {
      * to it and associates adapter to the recycler view
      */
     private void initializeDisplayContent() {
-        final RecyclerView recyclerView = findViewById(R.id.list_notes);
-        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView = findViewById(R.id.list_notes);
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        mGridLayoutManager = new GridLayoutManager(this, 2);
+
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         mNoteRecyclerAdapter = new NoteRecyclerAdapter(this,notes);
-        recyclerView.setAdapter(mNoteRecyclerAdapter);
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        mCoursesRecyclerAdapter = new CoursesRecyclerAdapter(this, courses);
+
+        showNotes();
     }
 
+    private void displayCourse(){
+        mRecyclerView.setAdapter(mCoursesRecyclerAdapter);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        checkNavigationMenuItem(R.id.nav_courses);
+    }
+    /**
+     * displays notes when notes section selected in drawer
+     */
+    private void showNotes() {
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mRecyclerView.setAdapter(mNoteRecyclerAdapter);
+        checkNavigationMenuItem(R.id.nav_notes);
+    }
+
+    private void checkNavigationMenuItem(int id){
+        NavigationView navigationView = findViewById(R.id.nav_bar);
+        Menu menu = navigationView.getMenu();
+        menu.findItem(id).setChecked(true);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.nav_notes){
+//            handleSelect("notes");
+            showNotes();
+        }else if(id == R.id.nav_courses){
+            handleSelect("courses");
+            displayCourse();
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void handleSelect(String notes) {
+        Snackbar.make(findViewById(R.id.list_notes),notes , Snackbar.LENGTH_LONG);
+    }
+
+    /**
+     * closes drawer if it is open when back button pressed
+     */
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
 }
